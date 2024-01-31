32b4548f6fa0614224a9e74daa9f1b0c22070b5e
Suryariski <suryariski508@gmail.com>
2024-01-31 10:20:47 +0800

    pemenang

--- application/views/admin/menu.php
+++ application/views/admin/menu.php
@@ -4,13 +4,13 @@
                                 <li class="treeview">
                                  <li>
                                     <a href="<?php echo site_url().'admin/index' ?>">
-                                        <i class="fa fa-book"></i> <span>Data user</span>
+                                        <i class="fa fa-book"></i> <span>Data User</span>
                                     </a>
                                 </li>
                                 <li class="treeview">
                                  <li>
-                                    <a href="<?php echo site_url().'admin/hadiah' ?>">
-                                        <i class="fa fa-book"></i> <span>Data Hadiah</span>
+                                    <a href="<?php echo site_url().'admin/pemenang' ?>">
+                                        <i class="fa fa-book"></i> <span>Data Pemenang</span>
                                     </a>
                                 </li>
                                 <!-- <li>
@@ -18,7 +18,6 @@
                                         <i class="fa fa-lock"></i> <span>Admin</span>
                                     </a>
                                 </li> -->
-                                
                                 <li>
                                     <a href="<?php echo site_url().'admin/tentang' ?>">
                                         <i class="fa fa-info"></i> <span>Tentang</span>
